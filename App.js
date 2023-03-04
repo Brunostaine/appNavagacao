16193068e3a2905c585f8ece44dd36acf83f18c3
Bruno Staine <brunostaine@hotmail.com>
2023-03-04 17:40:26 -0300

    inicio de navegação

--- App.js
+++ App.js
@@ -1,58 +1,28 @@
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Icon from 'react-native-vector-icons/FontAwesome'
-import IconFeather from 'react-native-vector-icons/Feather'
+import Home from "./src/pages/Home";
+import Sobre from "./src/pages/sobre";
 
-export default function App(){
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Sujeito programador</Text>
-      <Icon
-        name="home"
-        size={30}
-        color="#121212"
-      />
+    <NavigationContainer>
 
-      <Icon 
-        name="user"
-        size={30}
-        color="#54a300"
-      />
-
-      <IconFeather 
-        name="gift"
-        size={30}
-        color="#7665ff"
-      />
-
-      <TouchableOpacity style={styles.btnYoutube}>
-        <Icon
-          name="youtube"
-          size={30}
-          color="#FFF"
-        />
-        <Text style={styles.btnText}>Acessar Canal</Text>
-      </TouchableOpacity>
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{ 
+            title: 'Tela de Inicio', 
+            headerStyle: { backgroundColor: '#121212' }, 
+            headerTintColor: '#FFF',
+            // Tirar o header
+            headerShown: false
+            }} />
+        <Stack.Screen name="Sobre" component={Sobre} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  btnYoutube: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-    borderRadius: 10,
-    backgroundColor: '#FF0000'
-  },
-  btnText: {
-    marginLeft: 10,
-    color: '#FFF'
-  }
-})
---
+++ src/pages/sobre/index.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+
+export default function Sobre(){
+    return (
+      <View style={styles.container}>
+        <Text>SOBRE</Text>
+      </View>
+    )
+  }
+  
+  const styles = StyleSheet.create ({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  })
